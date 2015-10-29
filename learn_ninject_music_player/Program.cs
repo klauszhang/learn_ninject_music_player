ede9c95ad46239fbb43f87114ed62de69df68228
Hao Zhang <klaushaozhang.note@gmail.com>
2015-10-30 11:36:04 +1300

    initial commit

---
+++ learn_ninject_music_player/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learn_ninject_music_player
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
