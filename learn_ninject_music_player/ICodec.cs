1d8d1823a7968a8ee6aa3e416f92d933f2167af9
Hao Zhang <klaushaozhang.note@gmail.com>
2015-10-30 11:36:14 +1300

    ICodec interface

---
+++ learn_ninject_music_player/ICodec.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace learn_ninject_music_player
+{
+  public interface ICodec
+  {
+    string Name { get; }
+    bool CanDecode(string extension);
+    Stream Decode(Stream inStream);
+  }
+}
